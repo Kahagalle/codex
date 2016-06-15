5ac7e899b9e7dc1a3e7d2a299887ce57629890e8
Kahagalle <nayomilive@gmail.com>
2016-06-15 18:41:52 +0530

    All challenges

---
+++ public/javascripts/challenges.js
@@ -0,0 +1,450 @@
+var ch = [
+    {
+        "no": 1,
+        "title": "Introduction to HTML",
+        "type": "HTML"
+    },
+    {
+        "no": 2,
+        "title": "Working with headers",
+        "type": "HTML"
+    },
+    {
+        "no": 3,
+        "title": "Working with paragraphs",
+        "type": "HTML"
+    },
+    {
+        "no": 4,
+        "title": "Introduction to unordered list",
+        "type": "HTML"
+    },
+    {
+        "no": 5,
+        "title": "Introduction to ordered list",
+        "type": "HTML"
+    },
+    {
+        "no": 6,
+        "title": "Adding images",
+        "type": "HTML"
+    },
+    {
+        "no": 7,
+        "title": "Introduction to ordered list",
+        "type": "HTML"
+    },
+    {
+        "no": 8,
+        "title": "Introduction to CSS",
+        "type": "CSS"
+    },
+    {
+        "no": 9,
+        "title": "Changing fonts",
+        "type": "CSS"
+    },
+    {
+        "no": 10,
+        "title": "Working with text",
+        "type": "CSS"
+    },
+    {
+        "no": 11,
+        "title": "Styling text",
+        "type": "CSS"
+    },
+    {
+        "no": 12,
+        "title": "Changing background color",
+        "type": "CSS"
+    },
+    {
+        "no": 13,
+        "title": "Adding a background image",
+        "type": "CSS"
+    },
+    {
+        "no": "14",
+        "title": "Border styling",
+        "type": "CSS"
+    },
+    {
+        "no": 15,
+        "title": "Border styling",
+        "type": "CSS"
+    },
+    {
+        "no": 16,
+        "title": "Border styling",
+        "type": "CSS"
+    },
+    {
+        "no": 17,
+        "title": "Margins",
+        "type": "CSS"
+    },
+    {
+        "no": 18,
+        "title": "Padding",
+        "type": "CSS"
+    },
+    {
+        "no": 19,
+        "title": "Dimension properties",
+        "type": "CSS"
+    },
+    {
+        "no": 20,
+        "title": "Link styling",
+        "type": "CSS"
+    },
+    {
+        "no": 21,
+        "title": "Outline styling",
+        "type": "CSS"
+    },
+    {
+        "no": 22,
+        "title": "Display properties",
+        "type": "CSS"
+    },
+    {
+        "no": 23,
+        "title": "Position",
+        "type": "CSS"
+    },
+    {
+        "no": 24,
+        "title": "Float",
+        "type": "CSS"
+    },
+    {
+        "no": 25,
+        "title": "Class selectors",
+        "type": "CSS"
+    },
+    {
+        "no": 26,
+        "title": "Element element  selectors",
+        "type": "CSS"
+    },
+    {
+        "no": 27,
+        "title": "ID selectors",
+        "type": "CSS"
+    },
+    {
+        "no": 28,
+        "title": "Element, element selectors",
+        "type": "CSS"
+    },
+    {
+        "no": 29,
+        "title": "Cursors",
+        "type": "CSS"
+    },
+    {
+        "no": 30,
+        "title": "Z-index",
+        "type": "CSS"
+    },
+    {
+        "no": 31,
+        "title": "Introduction to JavaScript",
+        "type": "JavaScript"
+    },
+    {
+        "no": 32,
+        "title": "JavaScript Variables",
+        "type": "JavaScript"
+    },
+    {
+        "no": 33,
+        "title": "JavaScript Functions",
+        "type": "JavaScript"
+    },{
+        "no": 34,
+        "title": "JavaScript OnClick event",
+        "type": "JavaScript"
+    },
+    {
+        "no": 35,
+        "title": "Local  and Global Variables",
+        "type": "JavaScript"
+    },
+    {
+        "no": 36,
+        "title": "Math Operators",
+        "type": "JavaScript"
+    },
+    {
+        "no": 37,
+        "title": "If Statement",
+        "type": "JavaScript"
+    },
+    {
+        "no": 38,
+        "title": "If/Else Statement",
+        "type": "JavaScript"
+    },
+    {
+        "no": 39,
+        "title": "Else if Statements",
+        "type": "JavaScript"
+    },
+    {
+        "no": 40,
+        "title": "Nested if Statements",
+        "type": "JavaScript"
+    },
+    {
+        "no": 41,
+        "title": "Complex conditions",
+        "type": "JavaScript"
+    },
+    {
+        "no": 42,
+        "title": "Switch Statement",
+        "type": "JavaScript"
+    },
+    {
+        "no": 43,
+        "title": "For Loop",
+        "type": "JavaScript"
+    },
+    {
+        "no": 44,
+        "title": "While Loop",
+        "type": "JavaScript"
+    },
+    {
+        "no": 45,
+        "title": "onMouseOver Event Handler",
+        "type": "JavaScript"
+    },{
+        "no": 46,
+        "title": "JavaScript Objects",
+        "type": "JavaScript"
+    },
+    {
+        "no": 47,
+        "title": "JavaScript Arrays",
+        "type": "JavaScript"
+    },
+    {
+        "no": 48,
+        "title": "Working with Arrays",
+        "type": "JavaScript"
+    },
+    {
+        "no": 49,
+        "title": "Working with Arrays",
+        "type": "JavaScript"
+    },
+    {
+        "no": 50,
+        "title": "Array Properties and Methods",
+        "type": "JavaScript"
+    },
+    {
+        "no": 51,
+        "title": "Array Join Method",
+        "type": "JavaScript"
+    },
+    {
+        "no": 52,
+        "title": "Array Pop Method",
+        "type": "JavaScript"
+    },
+    {
+        "no": 53,
+        "title": "Array Push Method",
+        "type": "JavaScript"
+    },
+    {
+        "no": 54,
+        "title": "Array Reverse Method",
+        "type": "JavaScript"
+    },
+    {
+        "no": 55,
+        "title": "Array Sort Method",
+        "type": "JavaScript"
+    },
+    {
+        "no": 56,
+        "title": "Prompt Method",
+        "type": "JavaScript"
+    },
+    {
+        "no": 57,
+        "title": "Math Objects",
+        "type": "JavaScript"
+    },
+    {
+        "no": 58,
+        "title": "Date Method",
+        "type": "JavaScript"
+    },
+    {
+        "no": 59,
+        "title": "Accessing Forms",
+        "type": "JavaScript"
+    },
+    {
+        "no": 60,
+        "title": "Forms Validation",
+        "type": "JavaScript"
+    },
+    {
+        "no": 61,
+        "title": "Introduction to Bootstrap",
+        "type": "Bootstrap"
+    },
+    {
+        "no": 62,
+        "title": "Grid System",
+        "type": "Bootstrap"
+    },
+    {
+        "no": 63,
+        "title": "Text Styling",
+        "type": "Bootstrap"
+    },
+    {
+        "no": 64,
+        "title": "Text Colors",
+        "type": "Bootstrap"
+    },
+    {
+        "no": 65,
+        "title": "Background Colors",
+        "type": "Bootstrap"
+    },
+    {
+        "no": 66,
+        "title": "Tables",
+        "type": "Bootstrap"
+    },
+    {
+        "no": 67,
+        "title": "Navigation Bar",
+        "type": "Bootstrap"
+    },
+    {
+        "no": 68,
+        "title": "Dropdown Menu",
+        "type": "Bootstrap"
+    },
+    {
+        "no": 69,
+        "title": "Wells",
+        "type": "Bootstrap"
+    },
+    {
+        "no": 70,
+        "title": "Alerts",
+        "type": "Bootstrap"
+    },
+    {
+        "no": 71,
+        "title": "Introduction to jQuery",
+        "type": "jQuery"
+    },
+    {
+        "no": 72,
+        "title": "Element Selector",
+        "type": "jQuery"
+    },
+    {
+        "no": 73,
+        "title": "Id Selector",
+        "type": "jQuery"
+    },
+    {
+        "no": 74,
+        "title": "Class Selector",
+        "type": "jQuery"
+    },
+    {
+        "no": 75,
+        "title": "Other Selectors",
+        "type": "jQuery"
+    },
+    {
+        "no": 76,
+        "title": "Other Selectors",
+        "type": "jQuery"
+    },
+    {
+        "no": 77,
+        "title": "jQuery Event methods",
+        "type": "jQuery"
+    },
+    {
+        "no": 78,
+        "title": "jQuery Event methods",
+        "type": "jQuery"
+    },
+    {
+        "no": 79,
+        "title": "jQuery Event methods",
+        "type": "jQuery"
+    },
+    {
+        "no": 80,
+        "title": "jQuery Event methods",
+        "type": "jQuery"
+    },
+    {
+        "no": 81,
+        "title": "jQuery Event methods",
+        "type": "jQuery"
+    },
+    {
+        "no": 82,
+        "title": "jQuery Event methods",
+        "type": "jQuery"
+    },
+    {
+        "no": 83,
+        "title": "jQuery Event methods",
+        "type": "jQuery"
+    },
+    {
+        "no": 84,
+        "title": "jQuery Event methods",
+        "type": "jQuery"
+    },
+    {
+        "no": 85,
+        "title": "Fading Methods",
+        "type": "jQuery"
+    },
+    {
+        "no": 86,
+        "title": "Content Methods",
+        "type": "jQuery"
+    },
+    {
+        "no": 87,
+        "title": "Attribute Method",
+        "type": "jQuery"
+    },
+    {
+        "no": 88,
+        "title": "CSS Method",
+        "type": "jQuery"
+    },
+    {
+        "no": 89,
+        "title": "Traversing - Siblings",
+        "type": "jQuery"
+    },
+    {
+        "no": 90,
+        "title": "Remove Method",
+        "type": "jQuery"
+    }
+];
